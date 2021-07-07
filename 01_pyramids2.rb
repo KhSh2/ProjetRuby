92b215c8942d5c7ca7906f4961478c6e6193b44d
MatthieuLzn <Matthieu.l94@hotmail.fr>
2021-07-07 14:40:14 +0200

    pair/impair add

--- 01_pyramids2.rb
+++ 01_pyramids2.rb
@@ -17,6 +17,7 @@ def full_pyramid
 
 end
 
+                        #------------- Losange ------------#
 
 def wtf_pyramid
      puts "Salut, bienvenue dans ma super pyramide !
@@ -28,40 +29,38 @@ def wtf_pyramid
      r = 0
      x = 1
      i = 1
-     if r < (number/2)
-        until r > (number/2)-1
-        groot = ( number - x )
-        puts (space * groot) + (stone * i)
-        i += 2
-        x += 1
-        r += 1
-            if r > (number/2)-1
-                while r < number
-                    groot = ( number - x )
-                    puts (space * groot) + (stone * i)
-                    i -= 2
-                    x -= 1
-                    r += 1
+     until (number)%2 != 0
+        puts "Un nombre impair j'ai dit !"
+        puts "Allez choisis bien cette fois"
+        print "> "
+        number = gets.to_i
+     end
+            if r < (number/2)
+                puts "Le Losange :"
+                until r > (number/2)-1
+                groot = ( number - x )
+                puts (space * groot) + (stone * i)
+                i += 2
+                x += 1
+                r += 1
+                    if r > (number/2)-1
+                        while r < number
+                            groot = ( number - x )
+                            puts (space * groot) + (stone * i)
+                            i -= 2
+                            x -= 1
+                            r += 1
+                        end
+                    end
+
                 end
             end
-
-        end
-     end
      
 
 
 end
 
 
-#if r > (number*2)
-    #while r > (number*2)
-    #groot = ( number - x )
-    #puts (space * groot) + (stone * i)
-    #i += 2
-    #x += 1
-    #r += 1
-    #end
-#end
 
 wtf_pyramid
         
